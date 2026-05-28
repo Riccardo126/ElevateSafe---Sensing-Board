64fbab1d47de24b0b8baedf353a8e28242a5e468
Lolpe02 <pellegrino.2007948@studenti.uniroma1.it>
2026-05-28 16:50:22 +0200

    logger originale

--- src/development/comm_task.cpp
+++ src/development/comm_task.cpp
@@ -5,8 +5,6 @@
 // --- TASK 2: Communication - sends data blocks ---
 void vCommTask(void *pvParameters) {
   CommData packet;
-  
-  uint32_t blocksSent = 0;
   debugPrintln("[CommTask] Started");  // <-- ADD THIS
   
   for (;;) {
@@ -20,14 +18,6 @@ void vCommTask(void *pvParameters) {
     if (received != pdPASS) {
       debugPrintln("[CommTask] Received no packet");
       continue;
-    }
-
-    if (DEBUG_MODE) {
-      //debugPrint("[CommTask] anomalyType=%u elevatorID=%u\n", packet.anomalyType, packet.elevatorID);
-    } else {
-      Serial.write(reinterpret_cast<const uint8_t*>(&packet), sizeof(packet));
-      blocksSent++;
-      debugPrint("[CommTask] Sent packet #%lu\n", blocksSent);
     }
   }
 }
